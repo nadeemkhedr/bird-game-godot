0d7af06659afa8cd0993d17757f4278925399a1a
Nadeem Khedr <nadeemkhedr@gmail.com>
2023-09-25 15:43:52 +0300

    ✨ feat: dynamically adding birds when they die

--- animal/animal.gd
+++ animal/animal.gd
@@ -1,7 +1,10 @@
 extends RigidBody2D
 
+var dead = false
+
 func _physics_process(delta):
 	update_debug_label()
+	$VisibleOnScreenNotifier2D.screen_exited.connect(on_screen_exited)
 
 
 func update_debug_label():
@@ -11,3 +14,15 @@ func update_debug_label():
 		Utils.vec2_to_str(linear_velocity)
 	]
 	SignalManager.on_update_debug_label.emit(s)	
+
+
+func die():
+	if dead:
+		return
+	dead = true
+	SignalManager.on_animal_died.emit()
+	queue_free()
+	
+
+func on_screen_exited():
+	die()
--- level/level.gd
+++ level/level.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
+@onready var animal_start = $AnimalStart
+
+var animal_scene = preload("res://animal/animal.tscn")
+
+
 func _ready():
 	SignalManager.on_update_debug_label.connect(on_update_debug_label)
-	
+	SignalManager.on_animal_died.connect(on_animal_died)
+	on_animal_died()
+
+
+
+func on_animal_died():
+	var animal = animal_scene.instantiate()
+	animal.global_position = animal_start.global_position
+	add_child(animal)
+		
 
 func on_update_debug_label(text: String):
 	$DebugLabel.text = text
