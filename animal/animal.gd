4122f22775d6e019df50245f53ecc080b25a0d3a
Nadeem Khedr <nadeemkhedr@gmail.com>
2023-09-26 13:40:34 +0300

    ✨ feat: finish animal dragging and releasing

--- animal/animal.gd
+++ animal/animal.gd
@@ -1,5 +1,13 @@
 extends RigidBody2D
 
+@onready var stretch_sound = $StretchSound
+@onready var launch_sound = $LaunchSound
+
+const DRAG_LIMIT_MAX = Vector2(0, 60)
+const DRAG_LIMIT_MIN = Vector2(-60, 0)
+const IMPULSE_MULT = 20
+
+
 var dead = false
 var dragging = false
 var released = false
@@ -17,16 +25,17 @@ func _ready():
 	$VisibleOnScreenNotifier2D.screen_exited.connect(on_screen_exited)
 	input_event.connect(on_input_event)
 
+
 func _physics_process(delta):
 	update_debug_label()
 	
-	if released:
-		pass
+	if released || dragging == false:
+		return
+		
+	if Input.is_action_just_released("drag"):
+		release_it()
 	else:
-		if dragging == false:
-			return
-		else:
-			drag_it()
+		drag_it()
 	
 
 
@@ -41,8 +50,26 @@ func drag_it():
 	last_drag_amount = (last_dragged_position - gmp).length()
 	last_dragged_position = gmp
 	
+	if last_drag_amount > 0 && stretch_sound.playing == false:
+		stretch_sound.play()
+	
 	dragged_vector = gmp - drag_start
+	dragged_vector.x = clampf(dragged_vector.x, DRAG_LIMIT_MIN.x, DRAG_LIMIT_MAX.x)
+	dragged_vector.y = clampf(dragged_vector.y, DRAG_LIMIT_MIN.y, DRAG_LIMIT_MAX.y)
 	global_position = start + dragged_vector
+
+
+func release_it(): 
+	dragging = false
+	released = true
+	freeze = false
+	apply_central_impulse(get_impulse())
+	stretch_sound.stop()
+	launch_sound.play()
+
+
+func get_impulse():
+	return dragged_vector * -1 * IMPULSE_MULT
 	
 	
 func die():
